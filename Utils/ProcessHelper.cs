24fc8f64b5fc1e02042f2e8c661e64b7459ccf2c
YuChia Hsia <80935597+hades9979@users.noreply.github.com>
2026-04-01 16:15:02 +0800

    Create ProcessHelper.cs file with process management utilities

---
+++ Utils/ProcessHelper.cs
@@ -0,0 +1,27 @@
+namespace WindowsDeviceManager.Utils
+{
+    // Starts a process with the specified name
+    public static void StartProcess(string processName)
+    {
+        // Implementation goes here
+    }
+
+    // Elevates a process to run with admin privileges
+    public static void ElevateProcess(string processName)
+    {
+        // Implementation goes here
+    }
+
+    // Checks if the current process is running as admin
+    public static bool IsRunAsAdmin()
+    {
+        // Implementation goes here
+        return false;
+    }
+
+    // Kills a process by its name
+    public static void KillProcessByName(string processName)
+    {
+        // Implementation goes here
+    }
+}
